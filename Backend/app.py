742ca98fcc0ce123b76b173445e2c1cdb3a75870
AayushNarwade <aayushnarwade10@gmail.com>
2025-12-06 22:52:18 +0530

    Deployment friendly

--- Backend/app.py
+++ Backend/app.py
@@ -123,4 +123,5 @@ def analytics():
 # RUN
 # -----------------
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
